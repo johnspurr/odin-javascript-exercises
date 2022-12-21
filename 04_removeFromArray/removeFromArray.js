3de6e5bb7363947c56874127e6f145d150e2dfdb
John Spurr <79884239+johnspurr@users.noreply.github.com>
2022-12-21 18:27:27 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removals) {
+    for (r of removals) {
+        let i = array.indexOf(r);
+        if (i > -1) {
+            array.splice(i, 1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
