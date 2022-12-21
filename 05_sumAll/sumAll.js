2cf12f22e873b292aa21a83cfab949988b711ab8
John Spurr <79884239+johnspurr@users.noreply.github.com>
2022-12-21 18:40:32 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    // Arguments must be non-negative numbers
+    if (typeof from !== "number" || typeof to !== "number" || from < 0 || to < 0) {
+        return 'ERROR';
+    }
 
+    // Handle descending ranges
+    if (from > to) {
+        [from, to] = [to, from];
+    }
+
+    sum = 0;
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
